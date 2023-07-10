4b10cf9d51230cda0772b4cbe0e74aad64a76371
SHIVENDRA8004 <jat13632@gmail.com>
2023-07-10 17:18:57 +0530

    DOne

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Body from "./components/layouts/Body";
+import Body from "./components/pages/Body";
 import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Body/>
+      <Navbar />
+      <Body />
     </div>
   );
 }
---
+++ src/components/pages/Body.js
@@ -0,0 +1,10 @@
+import Sidebar from "../layouts/Sidebar";
+const Body = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Body;
